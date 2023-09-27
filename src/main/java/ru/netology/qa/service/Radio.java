96d002a29c44506097c24e87b4e703e2b3ad6e7b
Mikhail <mikle0240@gmail.com>
2023-09-27 14:49:57 +0300

    Modified Radio.Java

--- src/main/java/ru/netology/qa/service/Radio.java
+++ src/main/java/ru/netology/qa/service/Radio.java
@@ -1,8 +1,8 @@
 package ru.netology.qa.service;
 
 public class Radio {
-    public int currentRadioStationNumber;
-    public int currentVolume;
+    private int currentRadioStationNumber;
+    private int currentVolume;
 
     public int getCurrentRadioStationNumber() {
         return currentRadioStationNumber;
